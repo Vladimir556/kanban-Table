ab465c30a31c054aff48c6382d502fcb6a128ceb
Смелов Владимир <svm@vt2b.ru>
2022-06-30 11:36:21 +0300

    decompose functions

--- src/components/Kanban/KanbanCard.jsx
+++ src/components/Kanban/KanbanCard.jsx
@@ -1,12 +1,83 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
+import CloseSVG from '../svg/CloseSVG';
 
-const kanbanCard = ({}) => {
+const KanbanCard = ({
+    provided,
+    snapshot,
+    columns,
+    setColumns,
+    column,
+    columnId,
+    item,
+    index
+    }) => {
+    
+    const [isAddBtnActive, setIsAddBtnActive] = useState(false)
+    const [cardName, setCardName] = useState(item.content)
+    const [cardText, setCardText] = useState(item.text)
+
+
+    const onUpdateCardHandler = () => {
+        if(cardName !== item.content || cardText!== item.text)
+        {
+            const newItems = columns[columnId].items.map(el => {
+                if(el.id == item.id)
+                    return {id:el.id, content:cardName,text:cardText}
+                else
+                    return el
+            })
+            setColumns({
+                ...columns,
+                [columnId]:{
+                    name: column.name,
+                    items: newItems ? newItems : []
+                }
+            })
+        }
+    }
+    
+    // обновление картчки при новых данных с сервера
+    useEffect( () => {
+        setCardName(item.content)
+        setCardText(item.text)
+    },[item])
+
     return (
-        <div>
-            
+        <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className='kanban_card'
+            style={{
+            ...provided.draggableProps.style
+            }}
+        >
+            <div className='kanban_card_delete'>
+                <CloseSVG onClick={() => {
+                    const newItems = columns[columnId].items.filter((elem) => elem.id !== item.id)
+                    setColumns({
+                        ...columns,
+                        [columnId]:{
+                            name:column.name,
+                            items:newItems
+                        }
+                    })
+                }}/>
+            </div>
+            {/* card title */}
+            <input type="text" value={cardName}
+            onChange={(e) => setCardName(e.currentTarget.value)}
+            onBlur={() => onUpdateCardHandler()}
+            />
+            <textarea type="textarea" className='kanban_card_text' value={cardText}
+            onChange={(e) => setCardText(e.currentTarget.value)}
+            onBlur={() => onUpdateCardHandler()}
+            />
+            <p className='kanban_card_created'>21.01.22 15:30</p>
         </div>
     );
 };
 
-export default kanbanCard;
+export default KanbanCard;
--- src/pages/KanBan.js
+++ src/pages/KanBan.js
@@ -4,7 +4,7 @@ import CloseSVG from '../components/svg/CloseSVG';
 import { v4 as uuidv4 } from 'uuid';
 import { AuthContext } from '../context';
 import axios from 'axios'
-
+import KanbanCard from '../components/Kanban/KanbanCard';
 const KanBan = () => {
 
     const [flag,setFlag] = useState(true)
@@ -86,6 +86,42 @@ const KanBan = () => {
             })
     }
     
+    const onColumnNameChangeHandler = (e, columnId) => {
+        setColumns({
+            ...columns,
+            [columnId]:{
+                name: e.currentTarget.value,
+                items: columns[columnId].items
+            }
+        })
+    }
+
+    const onColumnDeleteClickHander = (columnId) => {
+        const newColumns = columns
+        delete newColumns[columnId]
+        setColumns({...newColumns})
+    }
+
+    const onColumnAddClickHander = () => {
+        setColumns({
+            ...columns,
+            [uuidv4()]:{
+                name:"new column",
+                items:[]
+            }
+        })
+    }
+
+    const onCardAddClickHander = (columnId) => {
+        setColumns({
+            ...columns,
+            [columnId]:{
+                name:columns[columnId].name,
+                items:[...columns[columnId].items, {id: uuidv4(), content:"New Card"}]
+            }
+        })
+    }
+    
     useEffect( () => {
         onPageLoaded()
         webSocketStart()
@@ -96,6 +132,7 @@ const KanBan = () => {
             if(socket.current.readyState == 1){
                 const message = {
                     id: 1337,
+                    username: username,
                     method: "kanban",
                     kanbanData: columns
                 }
@@ -133,21 +170,9 @@ const KanBan = () => {
                                     {/* одна колонка */}
                                 <div style={{ margin: 8, height: '100%'}} className="kanban_column"> 
                                     <p style={{userSelect: "none"}} className='kanban_column_name'><input type='text' value={column.name}
-                                        onChange={(e) => {
-                                            setColumns({
-                                                ...columns,
-                                                [columnId]:{
-                                                    name: e.currentTarget.value,
-                                                    items: columns[columnId].items
-                                                }
-                                            })
-                                        }}
+                                        onChange={(e) => onColumnNameChangeHandler(e, columnId)}
                                     />
-                                    <CloseSVG fill='#777' onClick={() => {
-                                        const newColumns = columns
-                                        delete newColumns[columnId]
-                                        setColumns({...newColumns})
-                                    }}/>
+                                    <CloseSVG fill='#777' onClick={() => onColumnDeleteClickHander(columnId)}/>
                                     </p>
                                     <Droppable droppableId={columnId} key={columnId}>
                                     {(provided, snapshot) => {
@@ -172,58 +197,21 @@ const KanBan = () => {
                                                 {(provided, snapshot) => {
                                                     return (
                                                         //! одна карточка
-                                                    <div
-                                                        ref={provided.innerRef}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                        className='kanban_card'
-                                                        style={{
-                                                        ...provided.draggableProps.style
-                                                        }}
-                                                    >
-                                                        <div className='kanban_card_delete'>
-                                                            <CloseSVG onClick={() => {
-                                                                const newItems = columns[columnId].items.filter((elem) => elem.id !== item.id)
-                                                                setColumns({
-                                                                    ...columns,
-                                                                    [columnId]:{
-                                                                        name:column.name,
-                                                                        items:newItems
-                                                                    }
-                                                                })
-                                                            }}/>
-                                                        </div>
-                                                        {/* card title */}
-                                                        <input type="text" 
-                                                        onClick={(e) => {
-                                                            console.log(columns[columnId].items[index].content)
-                                                            console.log(columns)
-                                                            console.log(columns[columnId].items)
-                                                            const newItems = columns[columnId].items.map(el => {
-                                                                if(el.id == item.id)
-                                                                    return {id:el.id, content:'new'}
-                                                                else
-                                                                    return el
-                                                            })
-                                                            console.log(newItems)
-                                                        }}
-                                                        />
-                                                        <textarea type="textarea" className='kanban_card_text'/>
-                                                    </div>
+                                                        <KanbanCard
+                                                        provided={provided}
+                                                        snapshot={snapshot}
+                                                        columns={columns}
+                                                        setColumns={setColumns}
+                                                        column={column}
+                                                        columnId={columnId}
+                                                        item={item}
+                                                        index={index}/>
                                                     );
                                                 }}
                                                 </Draggable>
                                             );
                                             })}
-                                            <div className='kanban_addItem' onClick={() => {
-                                                setColumns({
-                                                    ...columns,
-                                                    [columnId]:{
-                                                        name:column.name,
-                                                        items:[...columns[columnId].items, {id: uuidv4(), content:"New Card"}]
-                                                    }
-                                                })
-                                            }}>
+                                            <div className='kanban_addItem' onClick={() => onCardAddClickHander(columnId)}>
                                                 <CloseSVG fill='#56b5b8'/>
                                             </div>
                                             {provided.placeholder}
@@ -236,15 +224,7 @@ const KanBan = () => {
                             );
                             })}
                             <div className='kanban_addtable'>
-                                <div className='kanban_addtable_item' onClick={() => {
-                                    setColumns({
-                                        ...columns,
-                                        [uuidv4()]:{
-                                            name:"new column",
-                                            items:[]
-                                        }
-                                    })
-                                }}>
+                                <div className='kanban_addtable_item' onClick={() => onColumnAddClickHander()}>
                                     <CloseSVG fill='#777'/>
                                 </div>
                             </div>
